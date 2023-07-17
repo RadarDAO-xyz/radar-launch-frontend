5b1a2065cd3c1e8ddb352621aff95b85efba0278
Marcus Pang <marcuspangyuyang@gmail.com>
2023-07-17 23:33:06 +0200

    chore: move auth token to .env

--- .webflowrc.js
+++ .webflowrc.js
@@ -2,6 +2,6 @@ module.exports = {
     host: 'https://api.webflow.com',
     rootDir: './devlink',
     siteId: '64548f6f8feacfafa79c9592',
-    authToken: '839aceabff13ba96e1b11bb5a8994e2c26aad7510ebd27fff7fadf6f01c0b0ed', // An environment variable is recommended for this field.
+    authToken: process.env.DEVLINK_AUTH_TOKEN, // An environment variable is recommended for this field.
     cssModules: true
 };
