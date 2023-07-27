87f3cce04647dd52332a96e1ac6779b434df3bef
Kevin Compton <kevin@wecreativeagency.com>
2023-07-27 12:34:43 -0700

    update

---
+++ pages/update-profile/index.tsx
@@ -0,0 +1,13 @@
+import { AdminNav } from "@/devlink"
+
+export default function UpdateProfile() {
+
+    return (
+        <div className="mt-24 max-w-screen-lg mx-auto">
+            <AdminNav />
+            <div className="border border-slate-200 rounded p-10 mb-10">
+
+            </div>
+        </div>
+    )
+}
