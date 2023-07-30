ac0a535ec7e508b6a835fae8b3f76b85f5ff3831
marcuspang-cliniserve <marcus.pang@cliniserve.de>
2023-07-30 23:42:21 +0200

    feat: hardcode title to all pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>RADAR Launch</title>
         <link rel="icon" href="/favicon.png" sizes="any" />
       </Head>
       <body>
