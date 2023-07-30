d9fa5d82b5b20999acb498f2e60ebeda70eae15b
marcuspang-cliniserve <marcus.pang@cliniserve.de>
2023-07-31 01:12:46 +0200

    feat: add mainnet contract id

--- constants/paper.ts
+++ constants/paper.ts
@@ -1,2 +1,2 @@
 export const GOERLI_CONTRACT_ID = "49234d94-5d7d-4094-8fe3-44bed9c0ecf4";
-export const MAINNET_CONTRACT_ID = "";
+export const MAINNET_CONTRACT_ID = "4a8f7c55-5c2d-45b5-b8c3-9624c7e1b85a";
