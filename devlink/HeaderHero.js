e9f756d902319fe8b53aea0f21719ade4aa6f82d
marcuspang <marcuspangyuyang@gmail.com>
2023-07-31 13:51:18 +0200

    fix: hero section

--- devlink/HeaderHero.js
+++ devlink/HeaderHero.js
@@ -9,6 +9,7 @@ import {
   MAINNET_CONTRACT_ADDRESS,
 } from "@/constants/address";
 import { chains } from "@/components/Web3Provider";
+import Link from "next/link";
 
 export function HeaderHero({
   as: _Component = _Builtin.Section,
@@ -24,13 +25,30 @@ export function HeaderHero({
 
   return (
     <_Component className="header-featured" tag="section">
-      <_Builtin.Block className="floating-down-arrow" tag="div">
+      <_Builtin.Block
+        className="floating-down-arrow flex justify-between container absolute right-[5%]"
+        tag="div"
+      >
         <_Builtin.Paragraph className="body-text larger">
-          {"see curated projects ↓"}
+          {"Curated projects dropping weekly ↓"}
           <br />
         </_Builtin.Paragraph>
+        <Link href="https://www.culture3.xyz/" target="_blank" className="flex">
+          <p className="curator-text">
+            august curated by
+            <br />
+          </p>
+          <img
+            className="logo"
+            loading="lazy"
+            width={56}
+            height="auto"
+            src="https://uploads-ssl.webflow.com/64548f6f8feacfafa79c9592/64a6a82851e7ea1f12599225_62c421c68db31c451cbecc30_c3full_off_black.svg"
+          />
+          <div className="arrow-diagonal">{"↗"}</div>
+        </Link>
       </_Builtin.Block>
-      <_Builtin.Block className="featured-project-tabs" tag="div">
+      <_Builtin.Block className="featured-project-tabs space-x-8" tag="div">
         <_Builtin.Block className="about-div home" tag="div">
           <_Builtin.Heading className="heading-5" tag="h1">
             {"SUPPORT IDEAS AND BUILDERS OF FUTURES YOU BELIEVE IN"}
--- devlink/VisionOfTheWeekProject.js
+++ devlink/VisionOfTheWeekProject.js
@@ -8,11 +8,11 @@ export function VisionOfTheWeekProject({ as: _Component = _Builtin.Block }) {
         {"Featured Project"}
       </_Builtin.Paragraph>
       <_Builtin.Block className="floating-weekly-featured full" tag="div">
-        {"✨ VISION OF THE WEEK✨"}
+        {"✨ DROP OF THE WEEK ✨"}
       </_Builtin.Block>
       <_Builtin.Block className="project-image" tag="div">
         <_Builtin.HtmlEmbed
-          className="video-html opacity-70 cursor-default"
+          className="video-html cursor-default"
           value="%3Cdiv%20class%3D%22wrapper%22%3E%0A%09%3Cdiv%20class%3D%22youtube%22%20data-embed%3D%22FFvIb1gQYa8%22%3E%0A%09%09%3Cdiv%20class%3D%22play-button%22%3E%3C%2Fdiv%3E%0A%09%3C%2Fdiv%3E%0A%3C%2Fdiv%3E%0A%0A%3Cstyle%3E%0A%0Ahtml%20%7B%0A%09background-color%3A%20%23f3f3f3%3B%0A%7D%0A.wrapper%20%7B%0A%09max-width%3A%20100%25%3B%0A%09margin%3A%200px%3B%0A%09padding%3A%200px%3B%0A%7D%0A%0A.youtube%20%7B%0A%09background-color%3A%20%23000%3B%0A%09position%3A%20relative%3B%0A%09padding-top%3A%2056.25%25%3B%0A%09overflow%3A%20hidden%3B%0A%09cursor%3A%20pointer%3B%0A%7D%0A.youtube%20img%20%7B%0A%09width%3A%20100%25%3B%0A%09top%3A%20-16.82%25%3B%0A%09left%3A%200%3B%0A%09opacity%3A%200.7%3B%0A%7D%0A.youtube%20.play-button%20%7B%0A%09width%3A%2090px%3B%0A%09height%3A%2060px%3B%0A%09background-color%3A%20%23333%3B%0A%09box-shadow%3A%200%200%2030px%20rgba(%200%2C0%2C0%2C0.6%20)%3B%0A%09z-index%3A%201%3B%0A%09opacity%3A%200.8%3B%0A%09border-radius%3A%206px%3B%0A%7D%0A.youtube%20.play-button%3Abefore%20%7B%0A%09content%3A%20%22%22%3B%0A%09border-style%3A%20solid%3B%0A%09border-width%3A%2015px%200%2015px%2026.0px%3B%0A%09border-color%3A%20transparent%20transparent%20transparent%20%23fff%3B%0A%7D%0A.youtube%20img%2C%0A.youtube%20.play-button%20%7B%0A%09cursor%3A%20pointer%3B%0A%7D%0A.youtube%20img%2C%0A.youtube%20iframe%2C%0A.youtube%20.play-button%2C%0A.youtube%20.play-button%3Abefore%20%7B%0A%09position%3A%20absolute%3B%0A%7D%0A.youtube%20.play-button%2C%0A.youtube%20.play-button%3Abefore%20%7B%0A%09top%3A%2050%25%3B%0A%09left%3A%2050%25%3B%0A%09transform%3A%20translate3d(%20-50%25%2C%20-50%25%2C%200%20)%3B%0A%7D%0A.youtube%20iframe%20%7B%0A%09height%3A%20100%25%3B%0A%09width%3A%20100%25%3B%0A%09top%3A%200%3B%0A%09left%3A%200%3B%0A%7D%0A%0A%3C%2Fstyle%3E"
         />
       </_Builtin.Block>
@@ -26,29 +26,26 @@ export function VisionOfTheWeekProject({ as: _Component = _Builtin.Block }) {
           }}
         >
           <_Builtin.Block className="div-block-97" tag="div">
-            <_Builtin.Paragraph className="featured-project-title">
-              {"TITLe"}
+            <_Builtin.Paragraph className="featured-project-title font-bolded text-2xl text-gray-400">
+              {"FUTURES DROP #1"}
             </_Builtin.Paragraph>
             <_Builtin.Block className="arrow-diagonal" tag="div">
               {"↗"}
             </_Builtin.Block>
           </_Builtin.Block>
         </_Builtin.Link>
-        <_Builtin.Block className="featured-project-bio" tag="div">
+        {/* <_Builtin.Block className="featured-project-bio" tag="div">
           <_Builtin.Paragraph className="project-byline">
             {"byline"}
           </_Builtin.Paragraph>
-        </_Builtin.Block>
+        </_Builtin.Block> */}
         <_Builtin.Block className="_10px-div" tag="div" />
-        <_Builtin.Block className="collect-wrapper main" tag="div">
-          <_Builtin.Block className="div-block-30" tag="div">
-            <_Builtin.Block className="small-text" tag="div">
-              {"3728 • supporters"}
-            </_Builtin.Block>
-            <_Builtin.Block className="_20px-width" tag="div" />
+        <_Builtin.Block className="collect-wrapper main flex justify-between" tag="div">
+          <_Builtin.Block className="small-text text-gray-400" tag="div">
+            {"0000 • supporters"}
           </_Builtin.Block>
-          <_Builtin.Block className="small-text" tag="div">
-            {"00:00:00"}
+          <_Builtin.Block className="text-xs text-gray-400" tag="div">
+            {"00d 00h 00m"}
           </_Builtin.Block>
         </_Builtin.Block>
       </_Builtin.Block>
