1810199f957641fa613e4b6fad6aed969c8d3103
Kevin Compton <kevin@wecreativeagency.com>
2023-08-01 12:05:46 -0500

    Update index.tsx

--- pages/pool/[id]/index.tsx
+++ pages/pool/[id]/index.tsx
@@ -1,7 +1,11 @@
 import { FundingPoolProjectHeader } from "@/devlink";
+import { ReadyToLaunchComponent } from "@/devlink/ReadyToLaunchComponent";
 
 export default function PoolPage({ params }: { params: { slug: string } }) {
   return (
-      <FundingPoolProjectHeader />
+    <>
+       <FundingPoolProjectHeader />
+       <ReadyToLaunchComponent />
+    </>
   );
 }
