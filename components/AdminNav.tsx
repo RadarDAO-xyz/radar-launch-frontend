65a899200b10f342c08880a35327dcf5e143392b
Kevin Compton <kevin@wecreativeagency.com>
2023-08-01 09:37:24 -0500

    Update AdminNav.tsx

--- components/AdminNav.tsx
+++ components/AdminNav.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 interface Props {
     isUpdateProfile?: boolean;
@@ -6,8 +7,8 @@ interface Props {
 export function AdminNav({isUpdateProfile = false}:Props) {
 
     return (
-        <div className="admin-nav-bar flex mb-20">
-            <img className="admin-founder-image" src="https://uploads-ssl.webflow.com/64548f6f8feacfafa79c9592/645f08315a0001a9b5c9c81c_Screenshot%202023-05-13%20at%2010.45.59.png" />
+        <div className="admin-nav-bar flex mb-10">
+            <img alt="founder avatar" className="admin-founder-image" src="https://uploads-ssl.webflow.com/64548f6f8feacfafa79c9592/645f08315a0001a9b5c9c81c_Screenshot%202023-05-13%20at%2010.45.59.png" />
             <div>
                 <h1>Founder Name</h1>
                 <p>0x...</p>
@@ -17,7 +18,7 @@ export function AdminNav({isUpdateProfile = false}:Props) {
                     <a className="admin-link" href="/updates">Share Update ↗</a>
                 )}
                 {!isUpdateProfile && (
-                    <a className="admin-link" href="/update-profile">Edit Profile ↗</a>
+                    <Link className="admin-link" href="/update-profile">Edit Profile ↗</Link>
                 )}
             </div>
         </div>
