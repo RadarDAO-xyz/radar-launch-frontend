09f13084b308be1dc1e177abe0e4c63c112fe119
marcuspang <marcuspangyuyang@gmail.com>
2023-08-06 23:50:51 +0200

    feat: add metadata endpoint

---
+++ pages/api/metadata/index.ts
@@ -0,0 +1,23 @@
+import { getProject } from "@/lib/backend";
+import { generateVideoThumbnail } from "@/lib/generateVideoThumbnail";
+import isTestnet from "@/lib/isTestnet";
+import { ethers } from "ethers";
+import { NextApiRequest, NextApiResponse } from "next";
+import { optimism, optimismGoerli } from "wagmi/chains";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Record<string, string>>
+) {
+  if (req.method !== "GET") {
+    return res.status(404).json({});
+  }
+  return res.status(200).json({
+    name: "RADAR Launch",
+    description: `Launch is the place where founders, builders, creators, and makers can bring ideas to market no matter their shape or size. It's the place to source the earliest adopters by growing a waitlist. Find collaborators by requesting specific skills and services. Crowdfund milestones, all onchain. And, of course, the reward is reciprocal. Early adopters will be able to build onchain reputation by showing support to the most innovative projects aligned with the futures they believe in.
+  
+    By unlocking early consumer ownership that's financial (with skin in the game), emotional (with a vision you believe in), and social (with visible, onchain attribution), we believe we can supercharge adoption and — in doing so — further accelerate the better futures. We believe the future is multiplayer. Come join us..`,
+    image: "https://radarlaunch.app/project-image.png",
+    external_link: "https://radarlaunch.app",
+  });
+}
