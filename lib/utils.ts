0ee3382cd2e2bc6a0bf03124333a7ea3c0efe9e3
Kevin Compton <kevin@wecreativeagency.com>
2023-08-07 16:38:27 -0700

    responsive

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,6 +8,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export const shortenAddress: (arg0: string) => string = (address) => {
+  if (!address) return "no address provided";
+  const start = address.slice(0, 6);
+  const end = address.slice(address.length - 4);
+  return `${start}...${end}`;
+ };
+
 export function getCountdown(date: Date) {
   const today = new Date();
 
