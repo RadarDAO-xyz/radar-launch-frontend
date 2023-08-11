6833b640177f4d0391ac0aad509973655f8b382a
marcuspang <marcuspangyuyang@gmail.com>
2023-08-11 18:00:30 +0200

    fix: sponsor logos on pool card

--- components/PoolCard.tsx
+++ components/PoolCard.tsx
@@ -23,7 +23,13 @@ export function PoolCard({
         src={hero_image || "/background1.png"}
         className={cn("what-if-block-image-header object-cover mx-auto mb-0")}
       />
-      <h2 className={cn("text-xl text-center pt-4 uppercase max-w-[140px] mx-auto")}>{title}</h2>
+      <h2
+        className={cn(
+          "text-xl text-center pt-4 uppercase max-w-[140px] mx-auto"
+        )}
+      >
+        {title}
+      </h2>
       <div className="flex justify-between w-full space-y-2 md:space-y-0 md:space-x-2 pt-2 px-2 flex-col md:flex-row">
         <div className="border rounded-lg p-4 w-full">
           <div className="small-text">{"Remaining Funding pool"}</div>
@@ -45,25 +51,24 @@ export function PoolCard({
           <Link href={`/pool/${_id}`}>SEE POOL</Link>
         </Button>
       </div>
-      {sponsors
-        .filter(
-          (sponsor) =>
-            typeof sponsor.logo === "string" && sponsor.logo !== undefined
-        )
-        .map((sponsor, index) => (
-          <Image
-            key={sponsor.name}
-            className={cn(
-              "absolute rounded-full top-[2%]",
-              index !== 0 ? `left-[calc(2%+${80 * index}px)]` : ""
-            )}
-            loading="lazy"
-            alt={sponsor.name + " logo image"}
-            width={77}
-            height={77}
-            src={sponsor.logo!}
-          />
-        ))}
+      <div className="absolute top-[2%] flex space-x-2">
+        {sponsors
+          .filter(
+            (sponsor) =>
+              typeof sponsor.logo === "string" && sponsor.logo !== undefined
+          )
+          .map((sponsor) => (
+            <Image
+              key={sponsor.name}
+              className={"rounded-full"}
+              loading="lazy"
+              alt={sponsor.name + " logo image"}
+              width={77}
+              height={77}
+              src={sponsor.logo!}
+            />
+          ))}
+      </div>
     </div>
   );
 }
