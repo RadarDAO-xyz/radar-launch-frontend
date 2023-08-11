3851336a10c225dc9c742540130eea57cbe565ce
marcuspang <marcuspangyuyang@gmail.com>
2023-08-11 11:03:08 +0200

    fix: build issue

--- components/AdminNav.tsx
+++ components/AdminNav.tsx
@@ -16,7 +16,7 @@ export function AdminNav({ isUpdateProfile = false, user }: Props) {
       />
       <div>
         <h1>{user?.name || "Your Name"}</h1>
-        <p>{user?.wallets?.[0].address || "0x..."}</p>
+        <p>{user?.wallets?.[0] || "0x..."}</p>
       </div>
       <div className="admin-links ml-auto">
         {/* {isUpdateProfile && (
