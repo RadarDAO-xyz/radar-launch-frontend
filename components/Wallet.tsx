6079ac712d0a58203e1594f800cd00cb6557cbf7
marcuspang <marcuspangyuyang@gmail.com>
2023-08-11 13:14:39 +0200

    feat: add dropdown menu

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -1,21 +1,64 @@
 import { useAuth } from "@/hooks/useAuth";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { useAccount, useEnsAddress, useEnsName } from "wagmi";
+import { shortenAddress } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
+import { chains } from "./Web3Provider";
+import Link from "next/link";
+import { useGetCurrentUser } from "@/hooks/useGetCurrentUser";
 
 export function Wallet() {
   const { login, logout, isLoggedIn } = useAuth();
+  const { address } = useAccount();
+  const { data: ensName } = useEnsName({
+    address,
+    chainId: chains[0].id,
+    enabled: address !== undefined,
+  });
+  const { data: currentUserData } = useGetCurrentUser();
+
+  if (isLoggedIn && address !== undefined && currentUserData !== undefined) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"ghost"}>
+            {ensName || shortenAddress(address)}
+            <ChevronDown />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            {ensName || shortenAddress(address)}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="cursor-pointer" asChild>
+            <Link href={`/profile/${currentUserData._id}`}>Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => logout()} className="cursor-pointer">
+            Log Out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
 
   return (
     <Button
       onClick={() => {
-        if (isLoggedIn) {
-          logout();
-        } else {
-          login();
-        }
+        login();
       }}
       variant={"ghost"}
     >
-      {isLoggedIn ? "Logout" : "Login ⚙"}
+      Login ⚙
     </Button>
   );
 }
