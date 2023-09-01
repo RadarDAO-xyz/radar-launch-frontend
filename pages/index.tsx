5dae3b35cbddaf73ba1ad3cc2c172eb6df4a3076
marcuspang <marcuspangyuyang@gmail.com>
2023-09-01 12:05:13 +0200

    fix: hide price for new project collection

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,7 +90,6 @@ export default function HomePage() {
                   {...project}
                   showSupporters
                   showMintEndDate
-                  showPrice
                 />
               ))}
           </div>
