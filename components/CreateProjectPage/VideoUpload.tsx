7e4491d53360a8f5bfc43e84b6f8dd086e236726
marcuspang <marcuspangyuyang@gmail.com>
2023-09-28 21:06:26 +0200

    feat: tidy up video upload

--- components/CreateProjectPage/VideoUpload.tsx
+++ components/CreateProjectPage/VideoUpload.tsx
@@ -4,6 +4,8 @@ import { useCallback, useMemo, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
+import { FileUpIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 export const VideoUpload = () => {
   const [video, setVideo] = useState<File | undefined>();
@@ -16,7 +18,15 @@ export const VideoUpload = () => {
   } = useCreateAsset(
     video
       ? {
-          sources: [{ name: video.name, file: video }] as const,
+          sources: [
+            {
+              name: video.name,
+              file: video,
+              storage: {
+                ipfs: true,
+              },
+            },
+          ] as const,
         }
       : null,
   );
@@ -27,7 +37,7 @@ export const VideoUpload = () => {
     }
   }, []);
 
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: {
       'video/*': ['*.mp4'],
     },
@@ -51,24 +61,41 @@ export const VideoUpload = () => {
 
   return (
     <>
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        <p>Drag and drop or browse files</p>
+      <div
+        {...getRootProps()}
+        className={cn(
+          'flex w-full flex-col items-center justify-center rounded-xl border-2 border-dashed border-gray-200 py-6',
+          isDragActive && 'border-gray-400 bg-slate-100',
+          video && 'bg-slate-100/50',
+        )}
+      >
+        <input type="file" {...getInputProps()} />
+        {video ? (
+          <div className="text-center">
+            <h4>{video.name}</h4>
+            <Button
+              onClick={() => {
+                createAsset?.();
+              }}
+              disabled={!createAsset || status === 'loading'}
+            >
+              {progressFormatted ? progressFormatted : 'Upload to IPFS'}
+            </Button>
+          </div>
+        ) : (
+          <>
+            <FileUpIcon width={30} height={30} className="mb-2" />
+            <h4 className="text-xl font-bold">Upload Video</h4>
+            <p className="mt-2 text-sm text-gray-600">
+              Only .mp4 files are supported
+            </p>
+          </>
+        )}
       </div>
 
-      {error?.message && <p>{error.message}</p>}
-
-      {video ? <p>{video.name}</p> : <p>Select a video file to upload.</p>}
-      {progressFormatted && <p>{progressFormatted}</p>}
-
-      <Button
-        onClick={() => {
-          createAsset?.();
-        }}
-        disabled={!createAsset || status === 'loading'}
-      >
-        Upload
-      </Button>
+      {error?.message && (
+        <p className="py-2 font-medium text-destructive">{error.message}</p>
+      )}
     </>
   );
 };
