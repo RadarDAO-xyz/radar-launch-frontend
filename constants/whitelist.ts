db59ab9cf14e5c282401c7fde0c88584ed1eb18a
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-01-24 12:18:05 +0800

    chore: tidy up env variables

--- constants/whitelist.ts
+++
@@ -1,5 +0,0 @@
-import { Address } from 'viem';
-
-export const WHITELISTED_ADDRESSES: Address[] = [
-  '0x7730B4Cdc1B1E7a33A309AB7205411faD009C106'
-];
--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    VITE_WEB3AUTH_CLIENT_ID: process.env.VITE_WEB3AUTH_CLIENT_ID,
     VITE_INFURA_KEY: process.env.VITE_INFURA_KEY,
     BACKEND_URL: process.env.BACKEND_URL,
     WHITELISTED_ADDRESSES: process.env.WHITELISTED_ADDRESSES,
